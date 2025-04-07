6fb482c5424f7cb212238f5a5023b75068187ef6
UMANG-2003 <umangkumar20003@gmail.com>
2025-04-07 11:08:56 +0530

    user model updated

--- src/app.js
+++ src/app.js
@@ -1,5 +1,22 @@
 import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
 const app = express();
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN, 
+    credentials: true,
+})); 
+
+app.use(express.json({limit:"20kb"}));
+app.use(express.urlencoded({extended:true, limit:"20kb"}));
+app.use(express.static('public'));
+
+app.use(cookieParser());
+
+
+
 
 
 app.get('/', (req, res) => {
@@ -7,4 +24,4 @@ app.get('/', (req, res) => {
     });
 
 
-export default app;
+export default app; 
