bea3668179b000e4e982d7c885b9ee5841703410
UMANG-2003 <umangkumar20003@gmail.com>
2025-04-08 23:38:55 +0530

    #

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,5 +1,5 @@
 import mongoose ,{Schema} from "mongoose";
-
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const videoSchema = new Schema({
   videoFile:{
@@ -45,8 +45,7 @@ const videoSchema = new Schema({
   timestamps: true
 });
 
-
-
+videoSchema.plugin(mongooseAggregatePaginate);
 
 const Video = mongoose.model("video", videoSchema);
 export default Video;
